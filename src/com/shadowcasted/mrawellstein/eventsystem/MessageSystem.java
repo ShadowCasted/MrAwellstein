0bdcbd6f53de37d2a2c20e8f09578b2fd3f048e5
MrAwellstein <mrawellstein@gmail.com>
2014-07-28 17:15:49 -0700

    Shared a Basic Event System That I Created
    The Message System is a clone of the Event System which will be used in
    Mr. Server, AKA the server being used in CloudConsole. The Event System
    works by loading a Listener by class, and then using it to listen to
    certain events,

---
+++ src/com/shadowcasted/mrawellstein/eventsystem/MessageSystem.java
@@ -0,0 +1,40 @@
+package com.shadowcasted.mrawellstein.eventsystem;
+
+import java.util.HashMap;
+
+public class MessageSystem {
+	public static void Listen(Object o){
+		try{
+			if(listeners.containsKey(o.getClass())){
+				listeners.get(o.getClass()).parse(o);
+			}else{
+				throw new NoAssignedListenerException("No Listener Assigned...");
+			}
+		}catch(Exception e){e.printStackTrace();}
+	}
+	
+	public static void addListener(Listener l){listeners.put(l.tolistenfor(), l);}
+	
+	public static void addListener(@SuppressWarnings("rawtypes") Class c) throws Exception{
+		Listener l = ((Listener)c.newInstance());
+		listeners.put(l.tolistenfor(),l);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private static HashMap<Class, Listener> listeners = new HashMap<Class, Listener>();	
+	
+	public static class NoAssignedListenerException extends RuntimeException{
+		private static final long serialVersionUID = 1L;
+		public NoAssignedListenerException(String string) {super(string);}
+	}
+	
+	public static interface Listener {
+		
+		@SuppressWarnings("rawtypes")
+		public Class tolistenfor();
+		
+		public void parse(Object o);
+		
+	}
+
+}
